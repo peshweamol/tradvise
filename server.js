3b4d776cf70a5975e231d0c85389a300bec1093f
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2021-01-17 12:25:38 -0800

    updates server.js for serving the client/build for production

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//use client build in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
-app.get("/",(req,res)=>{
-  res.send("Hello World");
-})
+// app.get("/",(req,res)=>{
+//   res.send("Hello World");
+// })
 
 app.listen(PORT, () => {
   console.log("Server started listening on port " + PORT);
